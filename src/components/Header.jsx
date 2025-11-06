faa150909e3ab57e549b7802a179d859d4e6f9b2
david-1910 <dovud1.1910@gmail.com>
2025-11-06 18:09:45 +0500

    next commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,16 @@ import logocation from "../assets/location.svg";
 
 export default function Header() {
     return (
-        <div className="relative min-h-[145px] bg-blue-950">
-            <header className="flex  flex-col md:flex-row justify-between items-center px-4 md:px-8 lg:px-36 py-4 md:py-6 text-white absolute top-0 left-0 w-full z-10 bg-transparent">
-                <img src={logo} alt="Silkway Capital Holding" className="h-16 md:h-20 lg:h-24 mb-4 md:mb-0" />
-                
+        <div className="relative bg-blue-950 h-[100px]  md:h-[145px] w-full">
+            <header className="flex  md:flex-row justify-between items-center px-4 md:px-8 lg:px-36 py-4 md:py-6 text-white w-full">
                 <nav className="flex flex-wrap justify-center gap-4 md:gap-6 lg:gap-10 text-xs md:text-sm mb-4 md:mb-0">
                     <a href="#" className="hover:text-gray-300 transition duration-200">О компании</a>
                     <a href="#" className="hover:text-gray-300 transition duration-200">Каталог</a>
                     <a href="#" className="hover:text-gray-300 transition duration-200">Продать</a>
                     <a href="#" className="hover:text-gray-300 transition duration-200">Контакты</a>
                 </nav>
+
+                <img src={logo} alt="Silkway Capital Holding" className="h-16 md:h-20 lg:h-24 mb-4 md:mb-0" />
 
                 <div className="flex flex-col sm:flex-row items-center gap-3 md:gap-8">
                     <button className="flex items-center gap-2 text-xs md:text-sm">
